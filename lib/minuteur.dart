7f9b64452dbe8c53cd6ae6a702a39d08de431f0f
Nooaah <noah.chtl@gmail.com>
2021-09-28 01:01:37 +0200

    3.3.2 Programmation de Stream

---
+++ lib/minuteur.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+class ModeleMinuteur {
+  String temps;
+  double pourcentage;
+  ModeleMinuteur(this.temps, this.pourcentage);
+}
+
+class Minuteur {
+  double _rayon = 1;
+  bool _estActif = true;
+  Duration _temps = Duration();
+  Duration _tempsTotal = Duration();
+
+  String retournerTemps(Duration t) {
+    String minutes = (t.inMinutes < 10)
+        ? '0' + t.inMinutes.toString()
+        : t.inMinutes.toString();
+    int numSecondes = t.inSeconds - (t.inMinutes * 60);
+    String secondes = (numSecondes < 10)
+        ? '0' + numSecondes.toString()
+        : numSecondes.toString();
+    String tempsFormate = minutes + ':' + secondes;
+    return tempsFormate;
+  }
+
+  Stream<ModeleMinuteur> stream() async* {
+    yield* Stream.periodic(Duration(seconds: 1), (int a) {
+      String temps;
+      if (this._estActif) {
+        _temps = _temps - Duration(seconds: 1);
+        _rayon = _temps.inSeconds / _tempsTotal.inSeconds;
+        if (_temps.inSeconds <= 0) {
+          _estActif = false;
+        }
+      }
+      temps = retournerTemps(_temps);
+      return ModeleMinuteur(temps, _rayon);
+    });
+  }
+}
